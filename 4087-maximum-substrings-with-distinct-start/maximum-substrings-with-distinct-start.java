2e51ccd41d46e6d3828f10fc6805d893739a5bab
rubalsingh3 <rubalsin2006@gmail.com>
2026-04-05 20:30:46 +0530

    Time: 31 ms (47.17%) | Memory: 47.9 MB (65.78%) - LeetSync

---
+++ 4087-maximum-substrings-with-distinct-start/maximum-substrings-with-distinct-start.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int maxDistinct(String s) {
+        HashSet<Character> set = new HashSet<>();
+        for(char c : s.toCharArray()) set.add(c);
+
+        return set.size();
+    }
+}
