b1213721a9ca5b3f0920d1b01985c17f1d8ca339
rubalsingh3 <rubalsin2006@gmail.com>
2026-08-18 11:15:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort a stack/sort-a-stack.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void sortStack(Stack<Integer> st) {
+        if(st.isEmpty()) return;
+        
+        int top = st.pop();
+        sortStack(st);
+        insert(st, top);
+    }
+    
+    public void insert(Stack<Integer> st, int x) {
+        if(st.isEmpty() || st.peek() <= x) {
+            st.push(x);
+            return;
+        }
+        
+        int top = st.pop();
+        insert(st, x);
+        st.push(top);
+    }
+}
