2794e7d75a086a00c565e638cc730cc2b3c908c5
rubalsingh3 <rubalsin2006@gmail.com>
2026-09-03 00:16:05 +0530

    Time: 1 ms (80.94%) | Memory: 43.5 MB (12.92%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -2,23 +2,21 @@ class Solution {
     public int numDecodings(String s) {
         int n = s.length();
         int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        return countWays(0, s, n, dp);
-    }
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
 
-    public int countWays(int i, String s, int n, int[] dp){
-        if(dp[i] != -1) return dp[i];
-        if(i == n) return dp[i] = 1;
-        if(s.charAt(i) == '0') return dp[i] = 0;
+        for(int i=2; i<=n; i++){
+            int oneDigit = Integer.valueOf(s.substring(i-1, i));
+            int twoDigit = Integer.valueOf(s.substring(i-2, i));
 
-        int res = countWays(i + 1, s, n, dp);
-
-        if(i+1 < n){
-            if(s.charAt(i) == '1' || s.charAt(i) == '2' && s.charAt(i+1) <= '6'){
-                res += countWays(i+2, s, n, dp);
-            }
+            if(oneDigit >= 1) dp[i] += dp[i-1];
+            if(twoDigit >= 10 && twoDigit <= 26) dp[i] += dp[i-2];
         }
-        return dp[i] = res;
-        
+        return dp[n];
     }
+
+    // public int countWays(String s, int n, int[] dp){
+    
+        
+    // }
 }
