caf22c94f9258a32fe9fb28755689969aac6b752
rubalsingh3 <rubalsin2006@gmail.com>
2026-09-10 22:25:52 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (26.96%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,24 +1,38 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
-        int[][] dp = new int[m][n];
-        for(int[] rows : dp) Arrays.fill(rows, -1);
-        return solve(0, 0, obstacleGrid, m, n, dp);
-    }
-    public int solve(int i, int j, int[][] grid, int m, int n, int[][] dp){
-        if(i >= m || i < 0 || j >= n || j < 0 || grid[i][j] == 1){
-            return 0;
-        }
-        if(i == m-1 && j == n-1){
-            return 1;
+    public int uniquePathsWithObstacles(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m+1][n+1];
+
+        // filling 1st row
+        for(int i = 0; i < n; i++){
+            if(i > 0 && grid[0][i-1] == 1 || grid[0][i] == 1){
+                dp[0][i] = 0; // 0 ways to reach
+                grid[0][i] = 1; // edge case : for filling future dp table 
+            } else{
+                dp[0][i] = 1;
+            }
         }
 
-        if(dp[i][j] != -1){
-            return dp[i][j];
+        // filling 1st col
+        for(int j = 0; j < m; j++){
+            if(j > 0 && grid[j-1][0] == 1 || grid[j][0] == 1){
+                dp[j][0] = 0; // 0 ways to reach
+                grid[j][0] = 1; // same edge case 
+            } else{
+                dp[j][0] = 1;
+            }
         }
-        int down = solve(i+1, j, grid, m, n, dp);
-        int right = solve(i, j+1, grid, m, n, dp);
-        return dp[i][j] = down + right;
+
+        // filling remaining table
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(grid[i][j] == 1) dp[i][j] = 0;
+                else dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        } 
+        return dp[m-1][n-1];
+    
     }
+
 }
