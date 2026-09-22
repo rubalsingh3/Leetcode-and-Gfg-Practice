1705e32602c8ba08913d5c5c32028b1f75beecbc
rubalsingh3 <rubalsin2006@gmail.com>
2026-09-23 00:00:24 +0530

    Time: 13 ms (45.06%) | Memory: 56.6 MB (47.04%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,16 +1,26 @@
 class Solution {
     public int change(int amount, int[] coins) {
         
-        // 1D dp tabulation
+        // 2D dp tabulation
         
-        int[] dp = new int[amount+1];
-        dp[0] = 1;
-         
-        for(int coin : coins){
-            for(int curr = coin; curr <= amount; curr++){
-                dp[curr] += dp[curr - coin];
+        int n = coins.length;
+        int[][] dp = new int[n+1][amount+1];
+
+        for(int i=0; i<=n; i++){
+            dp[i][0] = 1;
+        }         
+        // fill table
+        for(int i = n-1; i >= 0; i--){
+            for(int curr = 1; curr <= amount; curr++){
+                // skip
+                dp[i][curr] = dp[i+1][curr];
+
+                // take
+                if(coins[i] <= curr){
+                    dp[i][curr] += dp[i][curr - coins[i]];
+                }
             }
         }
-        return dp[amount];
+        return dp[0][amount];
     }
 }
