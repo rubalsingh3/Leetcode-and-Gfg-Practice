3aa15eefd8c55fc2165b721cec3bc138591be7ce
rubalsingh3 <rubalsin2006@gmail.com>
2026-09-22 23:34:41 +0530

    Time: 40 ms (5.22%) | Memory: 57.6 MB (8.58%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -2,19 +2,23 @@ class Solution {
     public int change(int amount, int[] coins) {
         int[][] dp = new int[coins.length][amount+1];
         for(int[] x : dp) Arrays.fill(x, -1);
-        
-        return solve(amount, coins.length-1, coins, dp);
+
+        return solve(0, amount, coins, dp);
     }
-    public int solve(int amount,int idx, int[] coins, int[][] dp){
-        if(idx == 0){
-            return (amount % coins[idx] == 0) ? 1 : 0;
+    public int solve(int index, int amount, int[] coins, int[][] dp){
+        if(amount == 0) return 1;
+        if(index == coins.length) return 0;
+        
+        if(dp[index][amount] != -1) return dp[index][amount];
+
+        int ways = 0;
+        // take
+        if(coins[index] <= amount){
+            ways += solve(index, amount - coins[index], coins, dp);
         }
-        if(dp[idx][amount] != -1) return dp[idx][amount];
-        int notTake = solve(amount, idx-1, coins, dp);
-        int take = 0;
-        if(coins[idx] <= amount){
-            take = solve(amount - coins[idx], idx, coins, dp);
-        }
-        return dp[idx][amount] = notTake + take;
+        // skip 
+        ways += solve(index + 1, amount, coins, dp);
+
+        return dp[index][amount] = ways;
     }
 }
